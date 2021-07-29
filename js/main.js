e7454c86079e22e8f942618d3196788acc41411f
Vito <vito95v@gmail.com>
2021-07-29 16:23:45 +0200

    Prime linee di codice

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// Descrizione:
+// Un alert() espone 5 numeri generati casualmente.
+// Da li parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+var arrayGenerato = [];
+
+// For per generare 5 numeri casuali
+for (let i = 1; i <= 5; i++) {
+  let numero = Math.round(Math.random() * 100);
+  arrayGenerato.push(numero);
+}
+console.log(arrayGenerato);
+alert(arrayGenerato);
+
+var risultato = setTimeout(checkNumeri, 3000);
+console.log("Hai indovinato: " + risultato + " numeri");
+
+// Funzioni
+function checkNumeri(array) {
+  let arrayUtente = [];
+  let numeroUtente = 0;
+  let numeriIndovinati = 0;
+
+  for (let i = 1; i <= 5; i++) {
+    numeroUtente = parseInt(prompt("Inserisci il " + i + " numero"));
+    arrayUtente.push(numeroUtente);
+    if (array.includes(numeroUtente)) {
+      numeriIndovinati++;
+    }
+  }
+  //Questo console.log mi serve solo per verificare la correttezza dell'esercizio
+  console.log(arrayUtente);
+  return numeriIndovinati;
+}
